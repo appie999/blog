ec081a46a369cc5295ba82081bccab10e081cc7d
soumia <kablaouisoumia75@gmai.com>
2025-04-24 17:54:51 +0100

    working on the post list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PostDetailComponent } from "./post-detail/post-detail.component";
+import { PostListComponent } from './post-list/post-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [PostDetailComponent],
+  imports: [PostDetailComponent, RouterOutlet, PostListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
