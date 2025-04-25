096ef49694edc92b6ecab48d81b409eac15fc98d
majd eddine oumanni <majdeddineoumanni@gmail.com>
2025-04-25 17:59:24 +0100

    handling the forms

--- src/app/component/add-article/add-article.component.ts
+++ src/app/component/add-article/add-article.component.ts
@@ -7,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './add-article.component.css'
 })
 export class AddArticleComponent {
-
+  contentCounter : number = 0;
+  descriptionCounter : number = 0;
+  contentLengthController(event : KeyboardEvent){
+    if(event.key == "Backspace" && this.contentCounter > 0){
+      this.contentCounter --;
+    }else{
+      this.contentCounter ++;
+    }
+  }
+  descriptionLengthController(event : KeyboardEvent){
+    if(event.key == "Backspace" && this.descriptionCounter > 0){
+      this.descriptionCounter --;
+    }else{
+      this.descriptionCounter ++;
+    }
+  }
 }
