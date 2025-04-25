9b93f771a1f8a4051e386c0fa5172d164cc83367
majd eddine oumanni <majdeddineoumanni@gmail.com>
2025-04-25 10:59:01 +0100

    finished working on the UI of the article form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { PostDetailComponent } from './component/post-detail/post-detail.component';
 import { HomeComponent } from './component/home/home.component';
 import { PostListComponent } from './component/post-list/post-list.component';
+import { AddArticleComponent } from './component/add-article/add-article.component';
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
   { path: 'post/:id', component: PostDetailComponent },
-  { path: 'list', component:  PostListComponent}
+  { path: 'list', component:  PostListComponent},
+  { path: 'addArticle', component: AddArticleComponent}
 ]
